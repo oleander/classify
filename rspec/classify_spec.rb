c96161d975ef70870077347d1356f77ca91e7acf
Linus Oleander <linus@oleander.nu>
2011-02-18 02:16:44 +0100

    Adding some more rspec related code

---
+++ rspec/classify_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Classify do
+  it "should return a constant" do
+    
+  end
+end
---
+++ rspec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'rspec'
+require 'classify'
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+end
