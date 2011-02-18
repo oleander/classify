4ac5c241233f4fec1f9e220509d6a1c540919812
Linus Oleander <linus@oleander.nu>
2011-02-18 02:58:26 +0100

    Adding some basic tests

--- lib/classify.rb
+++ lib/classify.rb
@@ -1,3 +1,18 @@
-module Classify
-  # Your code goes here...
+class Classify
+  def self.it!(string)
+    raise ArgumentError, "Wrong argument #{string.inspect}" unless string.class == String
+    Classify.new.camelize(string)
+  end
+  
+  def try_converting(string)
+    unless /\A(?:::)?([A-Z]\w*(?:::[A-Z]\w*)*)\z/ =~ string
+      raise ArgumentError, "#{string} is not a valid constant name!"
+    end
+
+    Object.module_eval("::#{$1}", __FILE__, __LINE__)
+  end
+
+  def camelize(string)
+    self.try_converting(string.gsub(/^(.{1})|\_.{1}/) { |s| s.gsub(/[^a-z0-9]+/i, '').capitalize })
+  end
 end
--- spec/classify_spec.rb
+++ spec/classify_spec.rb
@@ -1,9 +1,32 @@
 require 'spec_helper'
 
-module MyString; end
+module Therandomstring; end
+module MyRandomString; end
+module A1; end
+module My1A; end
 
 describe Classify do
   it "should return a constant" do
-    Classify.it!("my_string").should eq(MyString)
+    Classify.it!("my_random_string").should eq(MyRandomString)
+    Classify.it!("a_1").should eq(A1)
+    Classify.it!("my_1_a").should eq(My1A)
+  end
+  
+  it "should raise an error if the param contains non valid data" do
+    ["my_stri::ng", "_value_some", "my_string ", "my_ö_ss", nil, Object, "", 1, "1_string"].each do |string|
+      lambda {
+        Classify.it!(string)
+      }.should raise_error(ArgumentError)
+    end
+  end
+  
+  it "should work without underscore" do
+    Classify.it!("therandomstring").should eq(Therandomstring)
+  end
+  
+  it "should work on a half done string" do
+    ["my_randomString", "myRandomString", "MyRandomString", "My_Random_String"].each do |string|
+      Classify.it!(string).should eq(MyRandomString)
+    end
   end
 end
